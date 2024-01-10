3f26fa9a10b8603a9cc42ddfde4de1f69251a205
Linh <lehoangnhatlinh0806@gmail.com>
2024-01-10 08:50:38 +0700

    update version tailwindcss

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import {useEffect, useRef} from 'react'
+import logo from '../../assets/images/logo.png'
+import { NavLink, Link } from 'react-router-dom'
+
+const navLink = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+
+  {
+    path: '/doctors',
+    display: 'Find a Doctor'
+  },
+
+  {
+    path: '/services',
+    display: 'Services'
+  },
+
+  {
+    path: '/contact',
+    display: 'Contact'
+  },
+]
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="header flex items-center">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            {/* ========= logo  ========= */}
+            <div>
+              <img src={logo} alt="" />
+              <h2 className='line-through'>Hihi</h2>
+            </div>
+
+            {/* ========= menu  ========= */}
+            <div className="navigation">
+              <ul className="menu flex items-center gap-[2.7rem]">
+                {navLink.map((item, index) => 
+                  <li key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) => {
+                        navClass.isActive
+                          ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                          : "text-textColor text-[16px] leading-7 font-[500]";
+                      }}
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header
