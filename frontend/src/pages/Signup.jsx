e00a5d6ecdd650b0425143d32ec0ef347d6aa090
lehaiyen223 <lhyen223@gmail.com>
2024-03-11 20:41:27 +0700

    Fix role

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,8 +5,6 @@ import { toast } from "react-toastify";
 import { BASE_URL } from "../../config";
 import signupImg from "../assets/images/signup01.png";
 import uploadImageToCloudinary from "../utils/uploadCloudinary";
-import { BASE_URL } from "../../config";
-import { toast } from "react-toastify";
 
 const Signup = () => {
   const [selectedFile, setSelectedFile] = useState(null);
